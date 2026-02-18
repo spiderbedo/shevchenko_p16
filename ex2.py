c6a67c31be05a892cd9805885af258b664c0c8bc
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-02-19 01:54:54 +0700

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,1 +1,21 @@
+def find_courses(students_courses) -> set:
+    '''
+    Finds the number of courses that every student chose
+    Args:
+        students_courses (list): Список множеств с курсами каждого студента
+    Return: set: Множество общих курсов
+    '''
 
+    if not students_courses:
+        return set()
+
+    common = students_courses[0].copy()
+    for courses in students_courses[1:]:
+        common &= courses
+    return common
+
+
+if __name__ == "__main__":
+    n = int(input())
+    all_courses = [set(input().split()) for _ in range(n)]
+    print(len(find_courses(all_courses)))
