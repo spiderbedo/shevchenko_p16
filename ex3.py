c2d3de51eab85e96b8b609b02ef668eab70306b0
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-02-19 02:08:11 +0700

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,1 +1,21 @@
+def only_sladkoezhkin() -> int:
 
+    '''
+    Finds how many products are liked only by Sladkoezhkin.
+    Args: None
+    Return: set: None
+    '''
+
+    sladkoezhkin = set(input().split())
+    n = int(input())
+    friends_products = set()
+
+    for _ in range(n):
+        friend = set(input().split())
+        friends_products |= friend
+
+    only_sladko = sladkoezhkin - friends_products
+    return len(only_sladko)
+
+if __name__ == "__main__":
+    print(only_sladkoezhkin())
