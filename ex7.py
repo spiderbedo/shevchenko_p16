cc3650c58348b42898c10c27af4f79e1b620d82e
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-02-19 02:56:37 +0700

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,1 +1,12 @@
+from itertools import permutations
 
+def all_permutations():
+    numbers_set = set(map(int, input().split()))
+    numbers = sorted(numbers_set)
+    perms = permutations(numbers)
+    for p in perms:
+        print(*p)
+
+
+if __name__ == "__main__":
+    all_permutations()
