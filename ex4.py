b8f5e0b6d4700629856535ce56125fd568e8d856
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-02-19 02:16:50 +0700

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,1 +1,20 @@
+def intersection() -> bool:
 
+    """
+    Finds out whether a given number exists in the intersection of two sets or not.
+    Args:
+        None
+    Returns:
+        bool
+    """
+  
+    fst_set = set(input().split())
+    snd_set = set(input().split())
+    value = input().strip()
+    inter = fst_set & snd_set
+    return value in inter
+
+
+if __name__ == "__main__":
+    result = intersection()
+    print(result)
