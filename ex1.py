e8dac93448ffc530e7a35d9b69c8a204d9c189c3
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-02-19 01:12:31 +0700

    Implement find_repeated function for duplicates check
    Added a function to check for repeated numbers in a sequence.

--- ex1.py
+++ ex1.py
@@ -1,1 +1,30 @@
+def find_repeated(numbers: str, needed_str: str) -> bool:
+  
+    '''
+    Checks whether the target number is among the repeating elements
+    in the sequence.
+    Args:
+        numbers
+        needed_str
+    Return: True if the number is repeated in the sequence, otherwise False.
+    '''
 
+    spisok = list(map(int, numbers.split()))
+    needed_num = int(needed_str)
+
+    seen = set()
+    duplicates = set()
+
+    for num in spisok:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+    return needed_num in duplicates
+
+
+if __name__ == "__main__":
+    input_nums = input()
+    input_needed = input()
+    result = find_repeated(input_nums, input_needed)
+    print(result)
