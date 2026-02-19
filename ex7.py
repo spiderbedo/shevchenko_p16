217730c77605d2559ef2ff98773a5adae603d3bb
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-02-19 09:24:44 +0700

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,6 +1,10 @@
 from itertools import permutations
 
 def all_permutations() -> None:
+    """
+    Reads a set of natural numbers and prints all their permutations
+    in lexicographical order.
+    """
     numbers_set = set(map(int, input().split()))
     numbers = sorted(numbers_set)
     perms = permutations(numbers)
