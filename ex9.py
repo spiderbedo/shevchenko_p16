44a86feac9f277d1acebf17d51725a22cac1b969
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-02-19 09:11:03 +0700

    Update ex9.py

--- ex9.py
+++ ex9.py
@@ -1,1 +1,20 @@
+from itertools import combinations
 
+def get_k_subsets() -> None:
+    """
+    Reads a set of natural numbers from input and a number K,
+    then prints subsets of len(k).
+    """
+    stroka = input().strip()
+    k_str = input().strip()
+    if not stroka or not k_str:
+        return None
+    elem = stroka.split()
+    k = int(k_str)
+    k_sub = list(combinations(elem, k))
+    for subset in k_sub:
+        print(list(subset))
+
+
+if __name__ == "__main__":
+    get_k_subsets()
